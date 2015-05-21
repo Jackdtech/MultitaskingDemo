2895d31037adf162ce3a9a29ee711207e804c370
Jack Douglas <Jackdtech@hotmail.com>
2015-05-21 06:20:28 -0700

    Initial Commit

---
+++ MultitaskingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultitaskingDemo
+//
+//  Created by Jack Douglas on 5/21/15.
+//  Copyright (c) 2015 Jack Douglas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
